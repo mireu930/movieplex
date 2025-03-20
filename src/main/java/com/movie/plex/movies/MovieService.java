cca007d53a6e4ab8e1b066cea9f97390637d0de9
yangtori0407 <heyang236@gmail.com>
2025-03-20 17:20:41 +0900

    feat: #6 tmdb API JSON 파싱 후 DB 저장 로직

---
+++ src/main/java/com/movie/plex/movies/MovieService.java
@@ -0,0 +1,15 @@
+package com.movie.plex.movies;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class MovieService {
+	
+	public void movieListJson() throws Exception{
+	}
+}
