3157ebcc640b4bb06a5168ff861758ed7498f348
mireu <mirue7367@gmail.com>
2025-03-26 22:29:58 +0900

    refactor: #25 jsp생성 및 cotroller수정

--- src/main/java/com/movie/plex/boards/notice/NoticeController.java
+++ src/main/java/com/movie/plex/boards/notice/NoticeController.java
@@ -2,10 +2,64 @@ package com.movie.plex.boards.notice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value = "/notice/*")
 public class NoticeController {
 
 	@Autowired
 	private NoticeService noticeService;
+	
+	@ModelAttribute("kind")
+	public String getKind() {
+		return "notice";
+	}
+	
+	@RequestMapping(value = "list", method = RequestMethod.GET)
+	public void getList() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "detail", method = RequestMethod.GET)
+	public void getDetail() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "add", method = RequestMethod.GET)
+	public void add() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "add", method = RequestMethod.POST)
+	public void add2() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "update", method = RequestMethod.GET)
+	public void update() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "update", method = RequestMethod.POST)
+	public void update2() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "delete", method = RequestMethod.GET)
+	public void delete() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "fileDelete", method = RequestMethod.POST)
+	public void fileDelete() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "fileDown", method = RequestMethod.GET)
+	public void fileDown() throws Exception {
+		
+	}
 }
--- src/main/java/com/movie/plex/boards/qna/QnaCotroller.java
+++ src/main/java/com/movie/plex/boards/qna/QnaCotroller.java
@@ -2,10 +2,64 @@ package com.movie.plex.boards.qna;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value = "/qna/*")
 public class QnaCotroller {
 	
 	@Autowired
 	private QnaService qnaService;
+	
+	@ModelAttribute("kind")
+	public String getKind() {
+		return "qna";
+	}
+	
+	@RequestMapping(value = "list", method = RequestMethod.GET)
+	public void getList() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "detail", method = RequestMethod.GET)
+	public void getDetail() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "add", method = RequestMethod.GET)
+	public void add() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "add", method = RequestMethod.POST)
+	public void add2() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "update", method = RequestMethod.GET)
+	public void update() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "update", method = RequestMethod.POST)
+	public void update2() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "delete", method = RequestMethod.GET)
+	public void delete() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "fileDelete", method = RequestMethod.POST)
+	public void fileDelete() throws Exception {
+		
+	}
+	
+	@RequestMapping(value = "fileDown", method = RequestMethod.GET)
+	public void fileDown() throws Exception {
+		
+	}
 }
